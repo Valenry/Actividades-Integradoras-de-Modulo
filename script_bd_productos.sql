ce08869e7ec9138315122c2b9b678b2f9d490cb3
Franco Culasso <131412072+FC892@users.noreply.github.com>
2023-06-11 19:27:12 -0300

    script_bd_productos.sql

--- script_bd_productos.sql
+++ script_bd_productos.sql
@@ -7,3 +7,33 @@ id_producto INT NOT NULL AUTO_INCREMENT,
 nombre VARCHAR(45) NOT NULL,
 PRIMARY KEY (id_producto))
 ENGINE = InnoDB;
+
+
+_________________________________________________________
+
+
+use actividad3;
+
+INSERT INTO productos (id_producto,nombre)VALUES
+('1','Pan');
+
+INSERT INTO productos (id_producto,nombre)VALUES
+('2','Medialunas');
+
+INSERT INTO productos (id_producto,nombre)VALUES
+('3','Criollos');
+
+INSERT INTO productos (id_producto,nombre)VALUES
+('4','Tortas');
+
+INSERT INTO productos (id_producto,nombre)VALUES
+('5','Tartas');
+
+INSERT INTO productos (id_producto,nombre)VALUES
+('6','Masas finas');
+
+INSERT INTO productos (id_producto,nombre)VALUES
+('7','Grisines');
+
+INSERT INTO productos (id_producto,nombre)VALUES
+('8','Croissants');
