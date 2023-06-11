59a32f39b4f371def342411a9d91bec3816cfd92
Valenry <131418384+Valenry@users.noreply.github.com>
2023-06-11 19:36:13 -0300

    Update script_bd_insumos.sql

--- script_bd_insumos.sql
+++ script_bd_insumos.sql
@@ -10,6 +10,23 @@ CREATE TABLE IF NOT EXISTS `insumos` (
 )
 ENGINE = InnoDB;
 
+-- INSERT Y SELECT TABLA INSUMOS
+
+use actividad3;
+INSERT INTO insumos (id_insumo, nombre, cantidad)
+VALUES 
+   (1, 'harina', '10 kg'),
+   (2, 'huevo', '12 unidades'),
+   (3, 'azucar', '5 kg'),
+   (4, 'margarina', '500 g'),
+   (5, 'leche', '1 litro'),
+   (6, 'sal', '1 kg'),
+   (7, 'levadura', '25 g'),
+   (8, 'manteca', '200 g'),
+   (9, 'esencia de vainilla', '100 ml');
+   
+   SELECT * FROM actividad3.insumos;
+
 
 
 
