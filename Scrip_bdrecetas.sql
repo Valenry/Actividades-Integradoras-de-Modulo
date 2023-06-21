58a1aca1552b3c3d9f0c303699e7d957245fea0f
anybelen <131079618+analiabelen@users.noreply.github.com>
2023-06-20 21:24:18 -0300

    Update Scrip_bdrecetas.sql

--- Scrip_bdrecetas.sql
+++ Scrip_bdrecetas.sql
@@ -1,59 +1,35 @@
+show databases;
+use big_bread_production;
+CREATE TABLE recetas (
+  id_receta INT PRIMARY KEY AUTO_INCREMENT,
+  id_producto INT,
+  id_insumo INT,
+  cantidad_requerida INT,
+  orden_preparacion INT,
+  FOREIGN KEY (id_producto) REFERENCES productos(id_producto),
+  FOREIGN KEY (id_insumo) REFERENCES insumos(id_insumo)
+);
+.....................................................................
+use big_bread_production;
 
--- Table `actividad3`.`recetas`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `actividad3`.`recetas` (
-  `id_recetas` INT NOT NULL AUTO_INCREMENT,
-  `insumos_id_insumo` INT NOT NULL,
-  `productos_id_producto` INT NOT NULL,
-  `cantidad_requerida` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id_recetas`),
-  CONSTRAINT `fk_recetas_insumo`
-    FOREIGN KEY (`insumos_id_insumo`)
-    REFERENCES `actividad3`.`insumos` (`id_insumo`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_recetas_productos1`
-    FOREIGN KEY (`productos_id_producto`)
-    REFERENCES `actividad3`.`productos` (`id_producto`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-)
-ENGINE = InnoDB
-DEFAULT CHARSET = utf8mb4
-COLLATE = utf8mb4_unicode_ci;
+INSERT INTO recetas (id_producto, id_insumo, cantidad_requerida, orden_preparacion, nombre_receta)
+VALUES
+  ((SELECT id_producto FROM productos WHERE codigo_barras = '1234567890' LIMIT 1), (SELECT id_insumo FROM insumos WHERE nombre = 'Harina' AND descripcion = 'Harina de trigo para uso en panadería' LIMIT 1), 500, 1, 'Receta de Pan'),
+  ((SELECT id_producto FROM productos WHERE codigo_barras = '1234567890' LIMIT 1), (SELECT id_insumo FROM insumos WHERE nombre = 'Agua' AND descripcion = 'Agua templada para uso en panadería' LIMIT 1), 250, 2, 'Receta de Pan'),
+  ((SELECT id_producto FROM productos WHERE codigo_barras = '1234567890' LIMIT 1), (SELECT id_insumo FROM insumos WHERE nombre = 'Levadura' AND descripcion = 'Levadura seca para uso en panadería' LIMIT 1), 10, 3, 'Receta de Pan'),
+  ((SELECT id_producto FROM productos WHERE codigo_barras = '1234567890' LIMIT 1), (SELECT id_insumo FROM insumos WHERE nombre = 'Azúcar' AND descripcion = 'Azúcar refinada para uso en panadería' LIMIT 1), 50, 4, 'Receta de Pan'),
+  ((SELECT id_producto FROM productos WHERE codigo_barras = '1234567890' LIMIT 1), (SELECT id_insumo FROM insumos WHERE nombre = 'Manteca' AND descripcion = 'Manteca animal para uso en panadería' LIMIT 1), 30, 5, 'Receta de Pan');
 
-CREATE INDEX `fk_recetas_insumo_idx` ON `actividad3`.`recetas` (`insumo_id_insumo` ASC) VISIBLE;
-CREATE INDEX `fk_recetas_productos1_idx` ON `actividad3`.`recetas` (`productos_id_producto` ASC) VISIBLE;
+INSERT INTO recetas (id_producto, id_insumo, cantidad_requerida, orden_preparacion, nombre_receta)
+VALUES
+  ((SELECT id_producto FROM productos WHERE codigo_barras = '2345678901' LIMIT 1), (SELECT id_insumo FROM insumos WHERE nombre = 'Harina' AND descripcion = 'Harina de trigo para uso en panadería' LIMIT 1), 800, 1, 'Receta de Torta'),
+  ((SELECT id_producto FROM productos WHERE codigo_barras = '2345678901' LIMIT 1), (SELECT id_insumo FROM insumos WHERE nombre = 'Azúcar' AND descripcion = 'Azúcar refinada para uso en panadería' LIMIT 1), 100, 2, 'Receta de Torta'),
+  ((SELECT id_producto FROM productos WHERE codigo_barras = '2345678901' LIMIT 1), (SELECT id_insumo FROM insumos WHERE nombre = 'Huevo' AND descripcion = 'Huevo de color para uso en panadería' LIMIT 1), 5, 3, 'Receta de Torta'),
+  ((SELECT id_producto FROM productos WHERE codigo_barras = '2345678901' LIMIT 1), (SELECT id_insumo FROM insumos WHERE nombre = 'Leche' AND descripcion = 'Leche de vaca para uso en panadería' LIMIT 1), 200, 4, 'Receta de Torta'),
+  ((SELECT id_producto FROM productos WHERE codigo_barras = '2345678901' LIMIT 1), (SELECT id_insumo FROM insumos WHERE nombre = 'Chocolate' AND descripcion = 'Chocolate blanco para uso en panadería' LIMIT 1), 150, 5, 'Receta de Torta');
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
-use actividad3;
-
-INSERT INTO recetas (insumo_id_insumo, productos_id_producto, cantidad_requerida)
-SELECT i.id_insumo, p.id_producto, '500 g'
-FROM insumos i
-JOIN productos p ON p.nombre = 'medialuna'
-WHERE i.nombre IN ('harina', 'manteca', 'azucar');
-
-
-INSERT INTO recetas (insumo_id_insumo, productos_id_producto, cantidad_requerida)
-SELECT i.id_insumo, p.id_producto, '1 kg'
-FROM insumos i
-JOIN productos p ON p.nombre = 'pan'
-WHERE i.nombre IN ('harina', 'sal', 'levadura');
-
-
-INSERT INTO recetas (insumo_id_insumo, productos_id_producto, cantidad_requerida)
-SELECT i.id_insumo, p.id_producto, '800 g'
-FROM insumos i
-JOIN productos p ON p.nombre = 'criollos'
-WHERE i.nombre IN ('harina', 'manteca', 'sal', 'levadura');
-
-
-INSERT INTO recetas (insumo_id_insumo, productos_id_producto, cantidad_requerida)
-SELECT i.id_insumo, p.id_producto, '1 kg'
-FROM insumos i
-JOIN productos p ON p.nombre = 'tartas'
-WHERE i.nombre IN ('harina', 'huevo', 'azucar', 'manteca');
+SELECT r.id_receta, p.nombre AS nombre_producto, i.nombre AS nombre_insumo, r.cantidad_requerida, r.orden_preparacion
+FROM recetas r
+JOIN productos p ON r.id_producto = p.id_producto
+JOIN insumos i ON r.id_insumo = i.id_insumo;
 
